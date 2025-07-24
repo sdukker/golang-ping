270ce6e12c4be24d28a264da309799c1b4ca0ce0
Sven Dukker <sven@ubiqu.com>
2025-07-24 14:03:30 +0200

    pong server

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func handlerPing(w http.ResponseWriter, r *http.Request) {
+func handlerPong(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Starting ping sequence!\n")
 
 	requestURL := os.Getenv("HEALTH_URL")
@@ -20,8 +20,8 @@ func handlerPing(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Printf("client: status code: %d\n", res.StatusCode)
 
-	w.Write([]byte("pong"))
-	fmt.Println("pong")
+	w.Write([]byte("ping"))
+	fmt.Println("ping")
 	fmt.Println(time.Now(), r.Method, r.RequestURI, "200")
 }
 
@@ -32,14 +32,14 @@ func handlerHealth(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/ping", handlerPing)
+	http.HandleFunc("/pong", handlerPong)
 	http.HandleFunc("/health", handlerHealth)
 
 	port := ":" + os.Getenv("PORT")
-	fmt.Printf("ping listening on 0.0.0.0, port %s\n", port)
+	fmt.Printf("pong listening on 0.0.0.0, port %s\n", port)
 	err := http.ListenAndServe(port, nil)
 
 	if err != nil {
-		fmt.Println("Error starting ping server: ", err)
+		fmt.Println("Error starting pong server: ", err)
 	}
 }
